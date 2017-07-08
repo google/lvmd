6f65570251165ca2d9c35c326bed6a96161c5c1c
Vladimir Pouzanov <farcaller@gmail.com>
2017-07-08 21:41:48 +0100

    Fixed duplicate credstore flag

--- main.go
+++ main.go
@@ -22,25 +22,13 @@ import (
 	"flag"
 
 	"github.com/golang/glog"
-	"github.com/google/credstore/client"
-	"github.com/google/go-microservice-helpers/client"
 	"github.com/google/go-microservice-helpers/server"
 	"github.com/google/go-microservice-helpers/tracing"
-	"github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-prometheus"
-	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
-	opentracing "github.com/opentracing/opentracing-go"
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
 	pb "github.com/google/lvmd/proto"
 	"github.com/google/lvmd/server"
-)
-
-var (
-	credStoreAddress = flag.String("credstore-address", "", "credstore grpc address")
-	credStoreCA      = flag.String("credstore-ca", "", "credstore server ca")
 )
 
 func main() {
@@ -53,34 +41,10 @@ func main() {
 	}
 
 	svr := server.NewServer()
-	var grpcServer *grpc.Server
 
-	if *credStoreAddress != "" {
-		appTok, err := client.GetAppToken()
-		if err != nil {
-			glog.Fatalf("failed to get app token: %v", err)
-		}
-		conn, err := clienthelpers.NewGRPCConn(*credStoreAddress, *credStoreCA, "", "")
-		if err != nil {
-			glog.Fatalf("failed to create connection to credstore: %v", err)
-		}
-		credStoreKey, err := client.GetSigningKey(context.Background(), conn, appTok)
-		if err != nil {
-			glog.Fatalf("failed to get signing key: %v", err)
-		}
-
-		glog.Infof("enabled credstore auth")
-		grpcServer = grpc.NewServer(
-			grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-				otgrpc.OpenTracingServerInterceptor(opentracing.GlobalTracer()),
-				grpc_prometheus.UnaryServerInterceptor,
-				client.CredStoreTokenInterceptor(credStoreKey),
-				client.CredStoreMethodAuthInterceptor(),
-			)))
-	} else {
-		grpcServer = grpc.NewServer(
-			grpc.UnaryInterceptor(
-				otgrpc.OpenTracingServerInterceptor(opentracing.GlobalTracer())))
+	grpcServer, _, err := serverhelpers.NewServer()
+	if err != nil {
+		glog.Fatalf("failed to init GRPC server: %v", err)
 	}
 
 	pb.RegisterLVMServer(grpcServer, &svr)
